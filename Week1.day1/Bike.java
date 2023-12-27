f6fe577cb49fe551a6a3ac1f4e1b910a4958f3ec
bals1995 <85060410+bals1995@users.noreply.github.com>
2023-12-27 21:14:08 +0530

    Add files via upload

---
+++ Week1.day1/Bike.java
@@ -0,0 +1,16 @@
+package week1_day1;
+
+public class Bike {
+    
+	public static void main(String[] args) {
+		Car obj1=new Car();
+		
+	    Bike obj2=new Bike();
+	    
+	    obj1.applyBreak();
+	    obj1.soundHorn();
+	    
+	
+	}
+
+}
